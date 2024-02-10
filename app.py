2030316065698fe859cc10351e3076aaf379a425
Blackbox3 <anuskasthacr7@gmail.com>
2024-02-10 14:12:59 +0545

    css added to project

--- app.py
+++ app.py
@@ -3,18 +3,18 @@ from keras.models import model_from_json
 from keras.preprocessing.image import img_to_array
 from PIL import Image
 import numpy as np
-import matplotlib.pyplot as plt
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
+
 
 # Load the model architecture from JSON file
-model_path = r"D:\Nepali Handwritten Character\ncrs\sample_model\model.json"
+model_path = r"D:\Nepali Handwritten Character\ncrs\static\sample_model\model.json"
 with open(model_path, "r") as json_file:
     loaded_model_json = json_file.read()
     loaded_model = model_from_json(loaded_model_json)
 
 # Load the weights into the model
-loaded_model.load_weights(r"D:\Nepali Handwritten Character\ncrs\sample_model\model (1).h5")
+loaded_model.load_weights(r"D:\Nepali Handwritten Character\ncrs\static\sample_model\model (1).h5")
 
 # Define a route for the homepage
 @app.route('/')
@@ -53,16 +53,7 @@ def predict():
     # Get the predicted character
     predicted_character = class_mapping[predicted_class]
 
-    # Convert image array back to image for visualization
-    img_array *= 255.0  # Reverse normalization
-    img_array = img_array.astype(np.uint8)  # Convert to uint8
-    image = Image.fromarray(img_array[0, :, :, 0], 'L')  # Convert to PIL Image
-
-    # Save the processed image
-    processed_image_path = 'static/processed_image.png'
-    image.save(processed_image_path)
-
-    return render_template('result.html', prediction=predicted_character, processed_image=processed_image_path)
+    return render_template('result.html', prediction=predicted_character)
 
 if __name__ == '__main__':
     app.run(debug=True)
